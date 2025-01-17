49486bfbc5fffc16bad551740c5878072c1ff920
Moaz Mohamed <131017117+TherealZoux@users.noreply.github.com>
2025-01-17 17:23:57 +0200

    Update index.js
    remove login and fav routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/login",
-      name: "login",
-      component: () => import('@/views/LoginView.vue'),
-    },
-    {
       path: "/",
       name: "home",
       component: () => import('@/views/HomeView.vue'),
@@ -20,11 +15,6 @@ const router = createRouter({
       name: "Dashboard",
       component: () => import('@/views/DashboardView.vue'),
 
-    },
-    {
-      path: "/favorite",
-      name: "favorite",
-      component: () => import('@/views/favoriteView.vue'),
     },
     {
       path: "/show/:id",
